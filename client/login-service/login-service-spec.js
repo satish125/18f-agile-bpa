8408c8d77f2639e52f0ea5457e5e9185eea02879
Juliet Sabol <julietsabol@gmail.com>
2015-06-18 16:54:26 -0400

    Add login service

---
+++ client/login-service/login-service-spec.js
@@ -0,0 +1,11 @@
+describe('loginService', function() {
+
+  beforeEach(module('web'));
+
+  it('should ...', inject(function(loginService) {
+
+	//expect(loginService.doSomething()).toEqual('something');
+
+  }));
+
+});
---
+++ client/login-service/login-service.js
@@ -0,0 +1,10 @@
+angular.module('web').factory('loginService',function($http) {
+
+	var loginService = {};
+
+	loginService.signIn = function(username, password){
+		return $http.get('api/login/'+username+'/'+password);
+	}
+
+	return loginService;
+});
