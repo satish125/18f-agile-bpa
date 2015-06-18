c90247080fef154a799d48866fc61cca0b60cda3
ttyson-CRGT <ttyson@crgt.com>
2015-06-18 17:07:56 -0400

    Changed URL for loginService

--- client/login-service/login-service.js
+++ client/login-service/login-service.js
@@ -3,7 +3,7 @@ angular.module('web').factory('loginService',function($http) {
 	var loginService = {};
 
 	loginService.signIn = function(username, password){
-		return $http.get('api/login/'+username+'/'+password);
+		return $http.get('api/bpa.php/loginService/'+username+'/'+password);
 	}
 
 	return loginService;
