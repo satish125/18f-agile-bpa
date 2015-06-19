ab0c53883e2eea991a4d3a0e0c010e6a496f0731
Lance Weitzel <lweitzel@crgt.com>
2015-06-18 21:10:33 -0400

    Remove trailing spaces for sonar issue

--- client/login-partial/login-spec.js
+++ client/login-partial/login-spec.js
@@ -7,12 +7,12 @@ describe('LoginCtrl', function() {
     beforeEach(inject(function($rootScope, $controller) {
       scope = $rootScope.$new();
       ctrl = $controller('LoginCtrl', {$scope: scope});
-    }));	
+    }));
 
 	it('should ...', inject(function() {
 
 		expect(1).toEqual(1);
-		
+
 	}));
 
 });
