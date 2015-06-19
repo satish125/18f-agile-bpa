d5c6130baaa426c2596e69fdb7cf8e921a7e5dc1
Ravi Jothimani <rjothimani@crgt.com>
2015-06-19 14:07:44 -0400

    added index to columns

--- server/sql/create_table_user_session.sql
+++ server/sql/create_table_user_session.sql
@@ -2,13 +2,16 @@ use 4840w;
 
 DROP TABLE IF EXISTS user_session;
 
-CREATE TABLE user_session(
+
+CREATE TABLE user_session (
   user_session_id INT NOT NULL AUTO_INCREMENT,
   user_id INT NULL,
   session_id VARCHAR(100) NULL,
   create_dttm DATETIME NULL,
   logout_dttm DATETIME NULL,
-  PRIMARY KEY (`user_session_id`));
+  PRIMARY KEY (user_session_id),
+  INDEX index_session_user_id (user_id ASC),
+  INDEX index_session_session_id (session_id ASC));
 
     
 COMMIT;
