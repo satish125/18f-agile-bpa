668369a8deb80070d20484170f9792209b5bcc1d
ttyson-CRGT <ttyson@crgt.com>
2015-06-19 14:37:59 -0400

    Changed API to not discern if a user_id or password was entered incorrectly, only that either one may be wrong.  Added validation to ensure that a value was provided for the user_id and password parameters.

--- server/api/index.php
+++ server/api/index.php
@@ -17,7 +17,7 @@ require 'Slim/Slim.php';
 
 ini_set('display_errors', '1');
 error_reporting(-1);
-	
+    
 $app = new Slim();
 
 $app->post('/loginUser', 'loginUser');
@@ -27,121 +27,125 @@ $app->run();
 
 class restResponse
 {
-	public $code = "";
-	public $msg = "";
-	public $payload = "";	
-	
-	function set($code_in, $msg_in, $payload_in)
+    public $code = "";
+    public $msg = "";
+    public $payload = "";    
+    
+    function set($code_in, $msg_in, $payload_in)
     {
         $this->code = $code_in;
-		$this->msg = $msg_in;
-		$this->payload = $payload_in;
+        $this->msg = $msg_in;
+        $this->payload = $payload_in;
     }
-	
-	function toJSON() {
-		echo json_encode($this);
-	}
-	
+    
+    function toJSON() {
+        echo json_encode($this);
+    }
+    
 }
 
 function loginUser() {
-	$response = new restResponse;
-	$session_id = session_id();
+    $response = new restResponse;
+    $session_id = session_id();
     try {
-		$request = Slim::getInstance()->request();
-		$body = json_decode($request->getBody());
-		
-		if (!property_exists($body, 'email')) {
-			$response->set("invalid_parameter","email parameter was not found", "");
-			return;
-		}
-		
-		if (!property_exists($body, 'password')) {
-			$response->set("invalid_parameter","password parameter was not found", "");
-			return;
-		}	
+        $request = Slim::getInstance()->request();
+        $body = json_decode($request->getBody());
+        
+        if (!property_exists($body, 'email')) {
+            $response->set("invalid_parameter","Email parameter was not found", "");
+            return;
+        }
+        
+        if (!property_exists($body, 'password')) {
+            $response->set("invalid_parameter","Password parameter was not found", "");
+            return;
+        }
+        
+        if ($body->email == "" OR $body->password == "") {
+            $response->set("missing_value","User ID and Password is required", "");
+            return;
+        }
+        
         $db = getConnection();
-		$sql = "SELECT user_id, password, zip, last_login FROM user WHERE email=:email";
+        $sql = "SELECT user_id, password, zip, last_login FROM user WHERE email=:email";
 
         $stmt = $db->prepare($sql);
         $stmt->bindParam("email", $body->email);
         $stmt->execute();
-		
+        
         $user_data = $stmt->fetchObject();
 
         if ($user_data == null) {
-			$response->set("user_not_found","User was not found", "");			
-		}	
-		else
-		{
-			if ($body->password == $user_data->password) {
-				try {
-					$sql = "update user set last_login= now() WHERE email=:email";
-					$stmt = $db->prepare($sql);
-					$stmt->bindParam("email", $body->email);
-					$stmt->execute();
+            $response->set("invalid_credentials","Invalid User ID and/or Password was entered", "");            
+        }    
+        else
+        {
+            if ($body->password == $user_data->password) {
+                try {
+                    $sql = "update user set last_login= now() WHERE email=:email";
+                    $stmt = $db->prepare($sql);
+                    $stmt->bindParam("email", $body->email);
+                    $stmt->execute();
 
-					$sql = "insert into user_session (user_id, session_id, create_dttm) values (:user_id, :session_id, now())";
-					$stmt = $db->prepare($sql);
-					$stmt->bindParam("user_id", $user_data->user_id);
-					$stmt->bindParam("session_id", $session_id);					
-					$stmt->execute();
-
-			//		$response->set("success","User was authenticated", array("SESSION_ID"=>$session_id) ;	
-					
-					$response->set("success","User was authenticated", array("SESSION_ID" => $session_id) );
-			
-				} catch(PDOException $e) {
-					$response->set("system_failure","System error occurred, unable to login", "");	
-				}
-			}
-			else
-			{
-				$response->set("invalid_password","Invalid password was entered", "");	
-			}
+                    $sql = "insert into user_session (user_id, session_id, create_dttm) values (:user_id, :session_id, now())";
+                    $stmt = $db->prepare($sql);
+                    $stmt->bindParam("user_id", $user_data->user_id);
+                    $stmt->bindParam("session_id", $session_id);                    
+                    $stmt->execute();
+                    
+                    $response->set("success","User was authenticated", array("SESSION_ID" => $session_id) );
+            
+                } catch(PDOException $e) {
+                    $response->set("system_failure","System error occurred, unable to login", "");    
+                }
+            }
+            else
+            {
+                $response->set("invalid_credentials","Invalid User ID and/or Password was entered", "");    
+            }
         } 
     } catch(PDOException $e) {
-		$response->set("system_failure","System error occurred, unable to login", "");	
+        $response->set("system_failure","System error occurred, unable to login", "");    
     } finally {
-		$db = null;
-		$response->toJSON();
-	}
+        $db = null;
+        $response->toJSON();
+    }
 }
 
 function getUser($email) {
-	$response = new restResponse;	
+    $response = new restResponse;    
 
     try {
-		$sql = "SELECT email, zip, password  FROM user WHERE email=:email";
+        $sql = "SELECT email, zip, password  FROM user WHERE email=:email";
         $db = getConnection();
         $stmt = $db->prepare($sql);
         $stmt->bindParam("email", $email);
         $stmt->execute();
         $user_data = $stmt->fetchObject();
         $db = null;
-		
+        
         if ($user_data == null) {
-			$response->set("user_not_found","User was not found", "");			
-		}	
-		else
-		{
-			$response->set("success","User was found", $user_data);
+            $response->set("user_not_found","User was not found", "");            
+        }    
+        else
+        {
+            $response->set("success","User was found", $user_data);
         }
     } catch(PDOException $e) {
-		$response->set("system_failure","System error occurred, unable to login", "");	
+        $response->set("system_failure","System error occurred, unable to login", "");    
     } finally {
-		$response->toJSON();
-	}	
+        $response->toJSON();
+    }    
 }
 
 function getConnection() {
-	$dbhost="127.0.0.1";
-	$dbuser="4840w";
-	$dbpass="4840w";
-	$dbname="4840w";
-	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	return $dbh;
+    $dbhost="127.0.0.1";
+    $dbuser="4840w";
+    $dbpass="4840w";
+    $dbname="4840w";
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);    
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
 }
 
 ?>
