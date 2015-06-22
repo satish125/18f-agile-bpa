15ef4712aacfbc0598d9b27e9697e22f70e16e8a
ttyson-CRGT <ttyson@crgt.com>
2015-06-22 10:35:56 -0400

    Implemented userLogin API in the userService

--- client/login-partial/login.js
+++ client/login-partial/login.js
@@ -1,6 +1,30 @@
-angular.module('web').controller('LoginCtrl',function($scope){
-	angular.element(document).ready(function () {
-        $.material.ripples('.btn');
-    });
+angular.module('web').controller('LoginCtrl', [ '$scope', '$stateParams', 'userService',
+    function($scope, $stateParams, userService) {
+        
+        $scope.loginInProgress = false;
+        $scope.userEmail = null;      
+        $scope.userPassword = null; 
+        $scope.errorMsg = null;
 
-});
+        $scope.user_session_model = null;        
+       
+        angular.element(document).ready(function () {
+            $.material.ripples('.btn');
+        });
+        
+        var loginUser = function() {
+            $scope.loginInProgress = true;
+            $scope.errorMsg = null;
+
+            userService.loginUser($scope.userEmail, $scope.userPassword).then(function(loginData) {
+                $scope.user_session_model = loginData;
+            }, function(error) {
+                $scope.errorMsg = error.message;
+            })['finally'](function() {
+                $scope.loginInProgress = false;
+            });
+        };
+
+    }
+
+]);
