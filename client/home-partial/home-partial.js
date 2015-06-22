d7f1bef7eb9f571fe8dd60c2d70b124c7f98ef7a
unknown <fzubko@CRGT-FP123F1.crgt.local>
2015-06-22 16:00:59 -0400

    Minor nav and css edits

--- client/home-partial/home-partial.js
+++ client/home-partial/home-partial.js
@@ -13,7 +13,13 @@ angular.module('web').controller('HomePartialCtrl',[ '$scope', '$state', '$state
         });
         
         recallService.recentRecalls($scope.dayLimit, $scope.recordLimit).then(function(recallData) {
-            $scope.recentRecalls_model = recallData.payload;
+            $scope.recentRecalls_model = recallData.payload.map(function(obj){
+            	obj.recall_initiation_date = new Date(
+            		obj.recall_initiation_date.substring(0,4),
+            		obj.recall_initiation_date.substring(4,6),
+            		obj.recall_initiation_date.substring(6,8));
+            	return obj;
+            });
             
             if (recallData.code !== "success") {
                 $scope.errorMsg = recallData.msg;
@@ -24,6 +30,52 @@ angular.module('web').controller('HomePartialCtrl',[ '$scope', '$state', '$state
             $scope.RecentRecallsInProgress = false;
         });
 
+        $scope.demoData = [
+	    {
+	        recall_number: "F-0489-2015",
+	        reason_for_recall: "Protein supplement fails to declare allergen: milk",
+	        status: "Ongoing",
+	        distribution_pattern: "Nationwide",
+	        product_quantity: null,
+	        recall_initiation_date: "yyyymmdd",
+	        state: "ME",
+	        event_id: "63260",
+	        product_type: "Food",
+	        product_description: "Hunger and Weight Vanilla  Dietary Supplement Packaged under the labels Stronger Faster Healthier Whey: 2 lb (32 oz),  and 10 oz sizes",
+	        country: "US",
+	        city: "Warren",
+	        recalling_firm: "Maine Natural Health, Inc.",
+	        report_date: "20141119",
+	        "@epoch": 1424553174.836488,
+	        voluntary_mandated: "Voluntary: Firm Initiated",
+	        classification: "Class I",
+	        code_info: "All lots codes that fail to declare the allergens: milk",
+	        "@id": "0015ee8ba126a8441eed062b89a6c1db7dee83a3f0fd5fed41c7d9022355b5ac",
+	        "openfda": {},
+	        initial_firm_notification: "Press Release"
+	    },
+	    {
+	        recall_number: "F-0489-2015",
+	        reason_for_recall: "Protein supplement fails to declare allergen: milk",
+	        status: "Ongoing",
+	        distribution_pattern: "Nationwide",
+	        product_quantity: null,
+	        recall_initiation_date: "1288323623006",
+	        state: "ME",
+	        event_id: "63260",
+	        product_type: "Food",
+	        product_description: "Hunger and Weight Vanilla  Dietary Supplement Packaged under the labels Stronger Faster Healthier Whey: 2 lb (32 oz),  and 10 oz sizes",
+	        country: "US",
+	        city: "Warren",
+	        recalling_firm: "Maine Natural Health, Inc.",
+	        report_date: "20141119",
+	        "@epoch": 1424553174.836488,
+	        voluntary_mandated: "Voluntary: Firm Initiated",
+	        classification: "Class I",
+	        code_info: "All lots codes that fail to declare the allergens: milk",
+	        "@id": "0015ee8ba126a8441eed062b89a6c1db7dee83a3f0fd5fed41c7d9022355b5ac",
+	        "openfda": {},
+	        initial_firm_notification: "Press Release"
+	    }];
     }
-    
-]);    
+]);
--- client/recall-service/recall-service.js
+++ client/recall-service/recall-service.js
@@ -9,7 +9,7 @@ angular.module('web').factory('recallService',['$q', '$http',
             var myDayLimit = (dayLimit === "") ? 30 : dayLimit;
             var myRecordLimit = (recordLimit === "") ? 100 : recordLimit;
             
-            $http.get("/api/recentRecalls/"+ myDayLimit + "/" + myRecordLimit ).then(function(response) {
+            $http.get("/api/recentRecalls/food/"+ myDayLimit + "/" + myRecordLimit ).then(function(response) {
                     deferred.resolve(response.data);
                 },
                 function(error) {
