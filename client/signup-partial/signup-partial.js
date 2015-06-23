519a908b9e8befd1eeda188d4bbc2d9e4cd26a71
unknown <fzubko@CRGT-FP123F1.crgt.local>
2015-06-23 08:20:01 -0400

    Adding ngFocus directive and some signup validation

--- client/signup-partial/signup-partial.js
+++ client/signup-partial/signup-partial.js
@@ -1,12 +1,14 @@
 angular.module('web').controller('SignupPartialCtrl',function($scope){
 
-	$scope.zipPattern = (function() {
+	$scope.zipPattern = /^\d{5}$/;
+
+	/*$scope.zipPattern = (function() {
 		var regexp = /^\d{5}$/;
 		return {
 			test: function(value) {
 				return regexp.test(value);
 			}
 		};
-	})();
+	})();*/
 
 });
