becd0780f4529d240bf8cc2aea2493613ee430c4
unknown <fzubko@CRGT-FP123F1.crgt.local>
2015-06-23 12:17:40 -0400

    Adding server response error handling for signup

--- client/signup-partial/signup-partial.js
+++ client/signup-partial/signup-partial.js
@@ -1,5 +1,5 @@
-angular.module('web').controller('SignupPartialCtrl',function($scope){
-	$scope.signup = {};
+angular.module('web').controller('SignupPartialCtrl',['$scope','$state','userService',function($scope,$state,userService){
+	$scope.signup = {errors: []};
 
 	$scope.passwordConfirmPattern = {
 		test: function(value) {
@@ -7,4 +7,26 @@ angular.module('web').controller('SignupPartialCtrl',function($scope){
 		}
 	};
 
-});
+	$scope.doSignup = function() {
+		$scope.signup.isInProgress = true;
+
+		userService.registerUser($scope.signup.email, $scope.signup.password)
+		.then(
+			function(data) {
+				$scope.userLogin_model = data.payload;
+
+				if (data.code === "success") {
+					$state.go('landing-partial');
+				} else {
+					$scope.signup.errors.push(data.msg);
+				}
+			},
+			function(error) {
+				$scope.signup.errors.push(error.message);
+			}
+		)['finally'](function(){
+			$scope.signup.isInProgress = false;
+		});
+	};
+
+}]);
