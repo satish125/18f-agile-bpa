d773079eb709696a91d2489dccee9b2d9e6665b6
unknown <fzubko@CRGT-FP123F1.crgt.local>
2015-06-23 15:49:27 -0400

    Connect page rough draft

--- client/stores-partial/stores-partial.js
+++ client/stores-partial/stores-partial.js
@@ -1,4 +1,50 @@
-angular.module('web').controller('StoresPartialCtrl',function($scope){
+angular.module('web').controller('StoresPartialCtrl',['$scope','productService',function($scope,productService){
 
+	$scope.stores = [
+		{
+			name: 'Amazon',
+			imageSrc: 'https://bagiq.com/images/store-logos/store-amazon.svg'
+		},
+		{
+			name: 'PeaPod',
+			imageSrc: 'https://bagiq.com/images/store-logos/store-peapod.png'
+		},
+		{
+			name: 'Walmart',
+			imageSrc: 'https://bagiq.com/images/store-logos/store-walmart.svg'
+		}
+	];
 
-});
+	function getStores(page){
+
+		productService.getStores(page)
+		.then(
+			function(data) {
+				$scope.stores = data.payload;
+				// if more pages available, go get them?
+				// getUserStores(++page);
+			},
+			function(error) {
+				$scope.errors.push(error.message);
+			}
+		);
+	}
+	getStores(1);
+
+	function getUserStores(page){
+
+		productService.getUserStores(page)
+		.then(
+			function(data) {
+				$scope.userStores = data.payload;
+				// if more pages available, go get them?
+				// getUserStores(++page);
+			},
+			function(error) {
+				$scope.errors.push(error.message);
+			}
+		);
+	}
+	getUserStores(1);
+
+}]);
