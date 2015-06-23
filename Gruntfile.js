3f48d71a0c8862f4c5adbc5b1c4ba0d4002c2323
Juliet Sabol <julietsabol@gmail.com>
2015-06-23 13:04:26 -0400

    Fix for updated karma tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -189,18 +189,18 @@ module.exports = function (grunt) {
     karma: {
       options: {
         frameworks: ['jasmine'],
-        files: [  //this files data is also updated in the watch handler, if updated change there too
-          '<%= dom_munger.data.appjs %>',
-          'bower_components/angular-mocks/angular-mocks.js',
-          createFolderGlobs('*-spec.js')
-        ],
         logLevel:'ERROR',
         reporters:['mocha'],
         autoWatch: false, //watching is handled by grunt-contrib-watch
         singleRun: true
       },
       all_tests: {
-        browsers: ['PhantomJS','Chrome','Firefox']
+        browsers: ['PhantomJS','Chrome','Firefox'],
+        files: [  //this files data is also updated in the watch handler, if updated change there too
+          {src:'<%= dom_munger.data.appjs %>'},
+          {src:'bower_components/angular-mocks/angular-mocks.js'},
+          {src:createFolderGlobs('*-spec.js')}
+        ]
       },
       during_watch: {
         browsers: ['PhantomJS']
