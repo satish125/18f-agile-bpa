6f5a51003589e37625b59c5b161c66af4468c4ed
cwoldorf <cwoldorf@crgt.com>
2015-06-25 18:09:11 -0400

    Created run script for db objects and added insert statement to user table.

---
+++ server/sql/run_mysql_scripts.sql
@@ -0,0 +1,8 @@
+source C:/GitHub/18f-agile-bpa/server/sql/create_4840w_db.sql;
+source C:/GitHub/18f-agile-bpa/server/sql/create_table_user.sql;
+source C:/GitHub/18f-agile-bpa/server/sql/create_table_user_product.sql;
+source C:/GitHub/18f-agile-bpa/server/sql/create_table_user_session.sql;
+source C:/GitHub/18f-agile-bpa/server/sql/create_table_api_key.sql;
+source C:/GitHub/18f-agile-bpa/server/sql/create_table_iamdata_properties.sql;
+source C:/GitHub/18f-agile-bpa/server/sql/insert_table_api_key.sql;
+source C:/GitHub/18f-agile-bpa/server/sql/insert_table_iamdata_properties.sql;
