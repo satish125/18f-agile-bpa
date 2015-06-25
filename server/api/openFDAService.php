58b09d8aa0b9c74dde4e46a550c70a6bdb1eb162
ttyson-CRGT <ttyson@crgt.com>
2015-06-25 10:31:37 -0400

    Updated matching service to score search results

--- server/api/openFDAService.php
+++ server/api/openFDAService.php
@@ -34,7 +34,7 @@ function openFDARecentRecalls($type, $days, $limit) {
         
         $response->set("success","Data successfully fetched from service", $bigArr["results"] );
     } catch(Exception $e) {
-        $response->set("system_failure", "System error occurred, unable to return data.", "");
+        $response->set("system_failure", "System error occurred, unable to return data", "");
     } finally {
         $response->toJSON();
     }
@@ -45,7 +45,23 @@ function openFDARecentRecalls($type, $days, $limit) {
  * match a purchase pulled in from Information Machine to FDA recall data
  * POST body - the purchase
  */
+
+function wordListParser($text, $wordListMap) {
+     
+    // Build array of search terms for product name, filter out common words and special characters
+    $wordList = array();        
     
+    foreach (explode(" ", $text) as &$value) {
+        if ( ! in_array($value, $wordListMap) ) {
+            $safeString=preg_replace('/[^A-Za-z0-9\-]/', '', $value);
+            if (! in_array($safeString, $wordList)) {                
+                array_push($wordList, $safeString);
+            }
+        }
+    }    
+    return $wordList;
+} 
+ 
 function openFDAProductMatch($type, $days) {
     $response = new restResponse;
     
@@ -58,7 +74,7 @@ function openFDAProductMatch($type, $days) {
     $words = "about,above,across,after,against,around,at,before,behind,below,beneath,beside,besides,between,beyond,".
              "by,down,during,except,for,from,in,inside,into,like,near,,off,out,outside,over,since,through,throughout,".
              "till,toward,under,until,up,upon,with,without,according,to,because,addition,front,place,regard,".
-             "spite,instead,on,account,the";
+             "spite,instead,on,account,the,and";
         
     $wordListMap = array_map('strtolower', explode(",", $words));
              
@@ -103,31 +119,13 @@ function openFDAProductMatch($type, $days) {
         $productName = str_replace('-', ' ', strtolower($productName));
         
         // Build array of search terms for product name, filter out common words and special characters
-        $productNamePieces = array();        
-
-        foreach (explode(" ", $productName) as &$value) {
-            if ( ! in_array($value, $wordListMap) ) {
-                $safeString=preg_replace('/[^A-Za-z0-9\-]/', '', $value);
-                if (! in_array($safeString, $productNamePieces)) {                
-                    array_push($productNamePieces, $safeString);
-                }
-            }
-        }
+        $productNamePieces = wordListParser($productName, $wordListMap);
         
         // Remove all hyphens in the product upc and convert to lower case
         $productUpc = str_replace('-', '', strtolower($productUpc));
-        
-        // Build array of search terms for upc code, filter out common words and special characters
-        $productUpcPieces = array();        
-        
-        foreach (explode(" ", $productUpc) as &$value) {
-            if ( ! in_array($value, $wordListMap) ) {
-                $safeString=preg_replace('/[^A-Za-z0-9\-]/', '', $value);
-                if (! in_array($safeString, $productUpcPieces)) {
-                    array_push($productUpcPieces, $safeString);
-                }
-            }
-        }
+
+        // Build array of search terms for product name, filter out common words and special characters
+        $productUpcPieces = wordListParser($productUpc, $wordListMap);
         
         //get openFDA api key
         $sql = "SELECT api_key FROM api_key WHERE service_name='OPEN_FDA'";
@@ -185,9 +183,57 @@ function openFDAProductMatch($type, $days) {
         $context  = stream_context_create($options);
         
         $result = file_get_contents($url, false, $context);
+        
         $bigArr = json_decode($result, true, 20);
         
-        $response->set("success","Data successfully fetched from service", $bigArr );
+        // If the API call has returned an error then capture it and return the code/message to the caller
+        if (array_key_exists('error',$bigArr)) {
+            $response->set($bigArr['error']['code'], $bigArr['error']['message'], "" );
+            return;
+        }
+        
+        // Exit with an error if the service did not contain a results array
+        if (!array_key_exists('results',$bigArr)) {
+            $response->set("results_failure","The api did not contain any results", "");
+            return;
+        }
+        
+        // Iterate thru each search result
+        foreach ($bigArr['results'] as $idx => $idxVal) {
+           
+            // Build array of terms for product name, filter out common words and special characters
+            if ( !array_key_exists('product_description', $idxVal) ) {
+                $resultProductNamePieces = array();   
+            } else {
+                $resultProductName =  str_replace('-', ' ', strtolower($idxVal['product_description']));
+                $resultProductNamePieces = wordListParser($resultProductName, $wordListMap);
+            }
+            
+            // Find matching terms for product upc
+            $matchingProductNamePieces = array_intersect ($productNamePieces, $resultProductNamePieces);
+            
+
+            // Build array of terms for product ups, filter out common words and special characters
+            if (!array_key_exists('code_info', $idxVal)) {
+                $resultProductUpcPieces = array();   
+            } else {
+                $resultProductUpc =  str_replace('-', ' ', strtolower($idxVal['code_info']));   
+                $resultProductUpcPieces = wordListParser($resultProductUpc, $wordListMap);
+            }
+            
+            // Find matching terms
+            $matchingProductUpcPieces = array_intersect ($productUpcPieces, $resultProductUpcPieces);
+            
+            // Calculating matching score
+            $matchingScore = ( count($matchingProductNamePieces) / count($resultProductNamePieces) ) + 
+                             ( count($matchingProductUpcPieces) / count($resultProductUpcPieces) );
+                          
+            $bigArr['results'][$idx]['matching_score']=round($matchingScore,2);
+            
+        }
+     
+        //var_dump ($bigArr);
+        $response->set("success","Data successfully fetched from service", $bigArr['results'] );
 
     } catch(Exception $e) {
         $response->set("system_failure", $e->getMessage(), "");
