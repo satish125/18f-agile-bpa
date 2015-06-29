f661087dbd7a96bf6c4aa4efdcf1f4aa1e6f5de2
ttyson-CRGT <ttyson@crgt.com>
2015-06-29 14:42:30 -0400

    Increased size of password field and updated default password for user of demo@crgt.com

--- server/sql/create_table_user.sql
+++ server/sql/create_table_user.sql
@@ -4,12 +4,12 @@ DROP TABLE IF EXISTS user;
 CREATE TABLE user (
 user_id int(11) NOT NULL AUTO_INCREMENT,
 email varchar(45) DEFAULT NULL,
-password varchar(45) NOT NULL,
+password varchar(65536) NOT NULL,
 zip int(11) NOT NULL,
 last_login datetime DEFAULT NULL,
 PRIMARY KEY (`user_id`),
 UNIQUE KEY email_UNIQUE (`email`)
 );
 
-insert into user (email, password, zip) values('demo@crgt.com', 'demodemo', '20151');
+insert into user (email, password, zip) values('demo@crgt.com', 'sha256:1000:38qWaNJiaOSoBHyxF6xd3cND6kuN8+BC:r9vD5Q2uD5Pp4Ndt0DuVg9g7M17KTNYf', '20151');
 COMMIT;
