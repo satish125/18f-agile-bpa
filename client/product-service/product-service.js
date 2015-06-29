3107afd497163296923d9d0de4b9610b78993696
Frank Zubko <frank.zubko@gmail.com>
2015-06-29 15:16:00 -0400

    Refactoring connect page

--- client/product-service/product-service.js
+++ client/product-service/product-service.js
@@ -1,9 +1,38 @@
 angular.module('web').factory('productService',['$q', '$http',
 
     function($q, $http) {
+        var userStoreMap = {};
+
         var service = {
 			stores: [],
 			userStores: []
+		};
+
+		var map = {
+			store: function(store){
+				store.isConnecting = false;
+				store.isDisconnecting = false;
+				store.isWorking = function(){return this.isConnecting || this.isDisconnecting;};
+				store.href = 'https://www.google.com/search?q='+encodeURIComponent(store.name)+'&btnI';
+				store.hasConnectionAttempt = function(){
+					return this.id in userStoreMap;
+				};
+				store.userStore = function(){
+					return userStoreMap[this.id] || {};
+				};
+				store.isConnected = function(){
+					return this.userStore().credentials_status === 'Verified';
+				};
+				return store;
+			},
+			userStore: function(userStore){
+				userStore.status = function(){
+					return this.credentials_status === 'Invalid' ? 'Invalid Credentials'
+						: this.credentials_status !== 'Verified' ? 'Connecting'
+						: this.scrape_status === 'Done' ? 'Ready' : 'Purchase Review';
+				};
+				return userStore;
+			}
 		};
 
         service.getUser = function() {
@@ -64,7 +93,7 @@ angular.module('web').factory('productService',['$q', '$http',
 				function(response) {
                     for(var resultIndex in response.data.payload){
 						// it's stored as {"0":x,"1":y}
-                        service.stores.push(response.data.payload[resultIndex]);
+                        service.stores.push(map.store(response.data.payload[resultIndex]));
                     }
                     response.data.payload = service.stores;
                     deferred.resolve(response.data);
@@ -88,7 +117,14 @@ angular.module('web').factory('productService',['$q', '$http',
 
             $http.get("/api/products/getUserStores/"+page).then(
 				function(response) {
-					Array.prototype.push.apply(service.userStores,response.data.payload.result);
+					Array.prototype.push.apply(service.userStores,response.data.payload.result.map(map.userStore));
+
+					// for ease of lookup
+					userStoreMap = {};
+					for (var i = 0; i < service.userStores.length; i++){
+						userStoreMap[service.userStores[i].supermarket_id] = service.userStores[i];
+					}
+
 					if (response.data.payload.meta.next_page) {
 						service.getUserStores(response.data.payload.meta.next_page);
 					}
@@ -133,6 +169,9 @@ angular.module('web').factory('productService',['$q', '$http',
             $http.post("/api/products/addUserStore", postData).then(
 				function(response) {
                     deferred.resolve(response.data);
+                    response.data.payload.result.supermarket_id = storeid; // success response object model doesn't match userStore fetch
+					service.userStores.push(response.data.payload.result); // add to the userStore list
+					userStoreMap[response.data.payload.result.supermarket_id] = response.data.payload.result;
                 },
                 function(error) {
                     deferred.reject(error);
@@ -150,6 +189,13 @@ angular.module('web').factory('productService',['$q', '$http',
             $http.delete("/api/products/deleteUserStore/" + userStoreId).then(
 				function(response) {
                     deferred.resolve(response.data);
+					for(var i = 0; i < service.userStores.length; i++){
+						if(service.userStores[i].id === userStoreId){
+							delete userStoreMap[service.userStores[i].supermarket_id];
+							service.userStores.splice(i,1); // remove from userStore list
+							break;
+						}
+					}
                 },
                 function(error) {
                     deferred.reject(error);
@@ -173,6 +219,15 @@ angular.module('web').factory('productService',['$q', '$http',
             $http.put("/api/products/updateUserStore", postData).then(
 				function(response) {
                     deferred.resolve(response.data);
+                    for(var i = 0; i < service.userStores.length; i++){
+						if(service.userStores[i].id === userStoreId){
+							// the payload result is only a string... "Updated"
+							// it doesn't return a store object like adding does
+							service.userStores[i].credentials_status = 'Not defined';
+							service.userStores[i].scrape_status = 'Not defined';
+							break;
+						}
+					}
                 },
                 function(error) {
                     deferred.reject(error);
--- client/stores-partial/stores-partial.js
+++ client/stores-partial/stores-partial.js
@@ -1,103 +1,54 @@
 angular.module('web').controller('StoresPartialCtrl',['$scope','productService',function($scope,productService){
 
-	$scope.encodeURIComponent = encodeURIComponent;
-
 	$scope.stores = [];
 
-	$scope.getUserStores = function(){
-		return productService.userStores.map(function(userStore){
-			userStore.status = function(){
-				return this.credentials_status === 'Invalid' ? 'Invalid Credentials'
-					: this.credentials_status !== 'Verified' ? 'Connecting'
-					: this.scrape_status === 'Done' ? 'Ready' : 'Purchase Review';
-			};
-			return userStore;
-		});
-	};
-
-	$scope.getUserStoreMap = function(){
-		var userStoreMap = {};
-		for (var i = 0; i < $scope.getUserStores().length; i++){
-			userStoreMap[$scope.getUserStores()[i].supermarket_id] = $scope.getUserStores()[i];
+	function setStores(){
+		$scope.stores = productService.stores;
+		if (productService.userStores.length){
+			orderStores();
 		}
-		return userStoreMap;
-	};
-
-	function buildScopeStores(){
-		$scope.stores = productService.stores.map(function(store){
-			store.isConnecting = false;
-			store.isDisconnecting = false;
-			store.isWorking = function(){return this.isConnecting || this.isDisconnecting;};
-			store.hasConnectionAttempt = function(){
-				return this.id in $scope.getUserStoreMap();
-			};
-			store.userStore = function(){
-				return $scope.getUserStoreMap()[this.id] || {};
-			};
-			store.isConnected = function(){
-				return this.userStore().credentials_status === 'Verified';
-			};
-			return store;
-		});
-		$scope.stores = $scope.$eval('stores | orderBy:[\'-hasConnectionAttempt()\',\'name\']'); // one-time orderBy
 	}
-
-	$scope.getStores = function(){
-		if ($scope.stores.length !== productService.stores.length){
-			buildScopeStores();
-		}
-		return $scope.stores;
-	};
+	function orderStores(){ // order is dependent on having both lists, stores and userStores
+		$scope.stores = $scope.$eval('stores | orderBy:[\'-hasConnectionAttempt()\',\'name\']'); // one-time orderBy on initial load
+	}
 
 	if (!productService.stores.length){
-		productService.getStores();
+		productService.getStores().then(setStores);
+	}else{ // it persisted in the service
+		setStores();
 	}
-	$scope.refreshUserStores = function(){
-		productService.getUserStores().then(buildScopeStores);
+
+	$scope.getUserStores = function(){
+		return productService.userStores;
 	};
-	$scope.refreshUserStores();
-	// rebuilding on every scope change
-	// future release - cleanup user-dependent data into the logout function if it persists in angular services
+
+	$scope.refreshUserStores = function(){
+		return productService.getUserStores();
+	};
+
+	$scope.refreshUserStores().then(function(){
+		if ($scope.stores.length) {
+			orderStores();
+		}
+	});
 
 	$scope.doStoreConnect = function(store){
 		var action = {};
 		store.isConnecting = true;
 		if (store.hasConnectionAttempt()){
-			action = productService.updateUserStore(store.userStore().id, store.username, store.password).then(function(response){
-				for(var i = 0; i < productService.userStores.length; i++){
-					if(productService.userStores[i].id === store.userStore().id){
-						// the payload result is only a string... "Updated"
-						// it doesn't return a store object like adding does
-						productService.userStores[i].credentials_status = 'Not defined';
-						productService.userStores[i].scrape_status = 'Not defined';
-					}
-				}
-			});
+			action = productService.updateUserStore(store.userStore().id, store.username, store.password);
 		}else{
-			action = productService.addUserStore(store.id, store.username, store.password).then(function(response){
-				response.payload.result.supermarket_id = store.id; // success response object model doesn't match userStore fetch
-				productService.userStores.push(response.payload.result); // add to the userStore list
-			});
+			action = productService.addUserStore(store.id, store.username, store.password);
 		}
-		action.finally(function(){store.isConnecting = false;});
+		action.finally(function(){
+			store.isConnecting = false;
+		});
 	};
 
 	$scope.doStoreDisconnect = function(store){
 		store.isDisconnecting = true;
 		productService.deleteUserStore(store.userStore().id)
-		.then(function(){
-			var userStoreId = store.userStore().id;
-			for(var i = 0; i < productService.userStores.length; i++){
-				if(productService.userStores[i].id === userStoreId){
-					productService.userStores.splice(i,1); // remove from userStore list
-					store.isDisconnecting = false;
-					return;
-				}
-			}
-		})
 		.finally(function(){
-			// redundancy in case of error
-			// kept original so userStore removal completed at the same time as the disconnect process
 			store.isDisconnecting = false;
 		});
 	};
