47a726d9f388d6ed69b5f762fe06602fc5e79177
Frank Zubko <frank.zubko@gmail.com>
2015-07-01 11:15:03 -0400

    Sonar fixes

--- client/recalls-partial/recalls-partial.js
+++ client/recalls-partial/recalls-partial.js
@@ -1,39 +1,27 @@
 angular.module('web').controller('RecallsPartialCtrl',['$scope', 'openfdaService',  'productService',
     function($scope, openfdaService, productService){
 
-        var dayLimit = 365;
-        var minScore = 0.6;
+        var dayLimit = 365, minScore = 0.6;
 
         function init(){
-            $scope.stores = null; //array of stores and purchases
-            $scope.matchResults = getCachedMatches(); //all results from open.fda
-            $scope.recalls = {}; //obj of purchase items with possible recalls
-
-            $scope.purchaseCount = 0; //number of items purchased
-            $scope.orderCount = 0; //number of purchase events
-            $scope.checkCount = 0; //number of purchase items that were successfully checked
-            $scope.attemptCount = 0; //number of purchase items that we've attempted to check
+            $scope.purchasesCollected = false;
+            $scope.matchResults = getCachedMatches();
+            $scope.recalls = {};
+            $scope.purchaseCount = 0;
+            $scope.checkCount = 0;
+            $scope.attemptCount = 0;
             $scope.progress = 0;
-
             getPageOfPurchases(1);
         }
 
         function getCachedMatches(){
-            var hasCache = !!localStorage['matches'];
-            var matchesDate = localStorage['matchesDate'];
-            var isExpired = matchesDate && (new Date() - new Date(matchesDate))/1000/60/60/24 > 1;
-
-            if(!hasCache || isExpired){
-                putCachedMatches({});
-            }
-
             try{
-                return JSON.parse(localStorage['matches']);
-            }catch(e){
-                putCachedMatches({});
-                return {};
-            }
-        }
+				if (localStorage['matchesDate'] && (new Date() - new Date(localStorage['matchesDate']))/1000/60/60/24 > 1){
+					return JSON.parse(localStorage['matches']) || {};
+				}
+			}catch(e){}
+			return {};
+		}
 
         function putCachedMatches(obj){
             localStorage['matches'] = JSON.stringify(obj);
@@ -41,7 +29,7 @@ angular.module('web').controller('RecallsPartialCtrl',['$scope', 'openfdaService
         }
 
         function setProgress(){
-            $scope.progress = ($scope.attemptCount/$scope.purchaseCount)*100;
+            $scope.progress = (++$scope.attemptCount/$scope.purchaseCount)*100;
         }
 
         $scope.recheckAll = function(){
@@ -53,92 +41,50 @@ angular.module('web').controller('RecallsPartialCtrl',['$scope', 'openfdaService
             return Object.keys(obj).length;
         };
 
-        /**
-         * @param page
-         */
         function getPageOfPurchases(page){
             return productService.getUserPurchases(dayLimit, page).then(function(response){
-                //instatiate an empty array, so we know we're done retrieving results
-                if($scope.stores === null){
-                    $scope.stores = [];
-                }
 
-                //no results found, leave the array empty
-                if(!response.result){
-                    $scope.progress = 100;
-                    return;
-                }
+                $scope.purchasesCollected = true;
 
-                //add this page of store purchases to our saved array
-                $scope.stores = $scope.stores.concat(response.result);
-
-                //loop through purchases and match
-                for(var i = 0, order; order = response.result[i]; i++){
-                    for(var j = 0, item; item = order.purchase_items[j]; j++){
-                        $scope.purchaseCount++;
-
-                        //pass the date to the php
-                        item.date = order.date;
-
-                        //pass the store to the product for display
-                        item.store = order.user_store.store_name;
-
-                        productMatch(item);
-                    }
-                }
-
-                //sum number of orders
-                $scope.orderCount += response.result.length;
-
-                //re-run this function if more pages to get
-                if(response.next_page){
-                    getPageOfPurchases(page+1);
+                if(response.result){
+					for(var i = 0, order; order = response.result[i]; i++){
+						for(var j = 0, item; item = order.purchase_items[j]; j++){
+							$scope.purchaseCount++;
+							productMatch($.extend(item,{date: order.date, store: order.user_store.store_name}));
+						}
+					}
+					if(response.next_page){
+						getPageOfPurchases(page+1);
+					}
                 }
             });
-        }//end getPageOfPurchases
+        }
 
         function productMatch(item){
-            var product = item.product;
-
-            //check cached recalls for the product
-            var cachedProduct = $scope.matchResults[product.id];
+            var cachedProduct = $scope.matchResults[item.product.id];
             if(typeof cachedProduct !== 'undefined'){
                 if(cachedProduct !== null){
-                    $scope.recalls[product.id] = cachedProduct;
+                    $scope.recalls[item.product.id] = cachedProduct;
                 }
                 $scope.checkCount++;
-                $scope.attemptCount++;
                 setProgress();
-                return;
-            }
+            } else {
+				openfdaService.productMatch(item, minScore).then(function(response){
+					if(response.code === 'success' || response.code === 'NO_MATCH'){
+						$scope.checkCount++;
+						if(response.payload.results.length > 0){
+							$scope.recalls[item.product.id] = response.payload;
+						}
+					}
+				}).finally(function(){
+					setProgress();
 
-            return openfdaService.productMatch(item, minScore).then(function(response){
-                //error
-                if(response.code !== 'success' && response.code !== 'NO_MATCH'){
-                    return;
-                }
+					$scope.matchResults[item.product.id] = $scope.recalls[item.product.id] ? $scope.recalls[item.product.id] : null;
 
-                //checked successfully
-                $scope.checkCount++;
-
-                //didn't find any matches
-                if(response.payload.results.length === 0){
-                    return;
-                }
-
-                //add recalls
-                $scope.recalls[product.id] = response.payload;
-            }).finally(function(){
-                //set progress
-                $scope.attemptCount++;
-                setProgress();
-
-                //cache the results
-                $scope.matchResults[product.id] = $scope.recalls[product.id] ? $scope.recalls[product.id] : null;
-
-                putCachedMatches($scope.matchResults);
-            });
-        }//end productMatch
+					putCachedMatches($scope.matchResults);
+				});
+			}
+        }
 
         $scope.toggleRecall = function(recall){
             recall.expanded = !recall.expanded;
