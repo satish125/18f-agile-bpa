09f95382df448d5f2abaf9f008bd441fc1fc06e2
Juliet Sabol <julietsabol@gmail.com>
2015-07-06 14:41:28 -0400

    Fix sharethis buttons

--- client/recall-directive/recall-directive.js
+++ client/recall-directive/recall-directive.js
@@ -1,7 +1,12 @@
 angular.module('web').directive('recallDirective', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs, fn) {
+			//make the sharethis buttons on the page
+			if(window.stButtons){
+				window.stButtons.makeButtons();
+			}
 
-	//make the sharethis buttons on the page
-	if(window.stButtons){
-		window.stButtons.makeButtons();
-	}
+		}
+	};
 });
